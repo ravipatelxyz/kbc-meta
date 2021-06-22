72261be3ff2e5752f262ea415151fcc3f896d000
Ravi Patel <79413022+ravipatelxyz@users.noreply.github.com>
2021-06-22 13:10:46 +0100

    metatoy2 setup with wandb and command line inputs with argparse

--- toymeta2/kbc-cli-toymeta2_higher.py
+++ toymeta2/kbc-cli-toymeta2_higher.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 
 # %%
+import argparse
+import sys
+import os
 
 import multiprocessing
 import numpy as np
@@ -13,7 +16,7 @@ import torch
 from torch import nn, optim
 
 import higher
-from kbc.util import set_seed
+import wandb
 
 torch.set_num_threads(multiprocessing.cpu_count())
 
@@ -27,24 +30,57 @@ class ToyMeta2(nn.Module):
     def forward(self, tensor_a):
         return tensor_a
 
-def main():
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser('KBC Research', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    # a,h,c starting vals
+    parser.add_argument('--a', action='store', required=True, type=float)
+    parser.add_argument('--h', action='store', required=True, type=float)
+    parser.add_argument('--c', action='store', required=True, type=float)
+
+    # inner loop
+    parser.add_argument('--optimizer', '-oi', action='store', type=str, default='adam',
+                        choices=['adam', 'adagrad', 'sgd'])
+    parser.add_argument('--learning_rate', '-li', action='store', type=float, default=0.01)
+    parser.add_argument('--inner_steps', '-is', action='store', type=int, default=100)
+
+    # outer loop
+    parser.add_argument('--optimizer_outer', '-oo', action='store', type=str, default='adam',
+                        choices=['adagrad', 'adam', 'sgd'])
+    parser.add_argument('--learning_rate_outer', '-lo', action='store', type=float, default=0.005)
+    parser.add_argument('--outer_steps', '-os', action='store', type=int, default=100)
+    parser.add_argument('--stopping_tol_outer', '-to', action='store', type=float, default=0.02)
+
+    # other
+    parser.add_argument('--save_figs', '-sf', action='store', type=str, default='False', choices=['True', 'False'])
+    parser.add_argument('--use_wandb', '-wb', action='store', type=str, default='False', choices=['True', 'False'])
+
+    return parser.parse_args(argv)
+
+
+def main(argv):
     """
     Model: Single parameter a that it returns
     Inner optimisation: min_a ||a-h||, with a the single model parameter, h being a hyperparameter
     Outer optimisation: min_p ||a-c||, with c set to a fixed value
     """
-    optimizer_name = OPTIMIZER
-    optimizer_outer_name = OPTIMIZER_OUTER
-    inner_steps = INNER_STEPS
-    outer_steps = OUTER_STEPS
-    learning_rate = LEARNING_RATE
-    learning_rate_outer = LEARNING_RATE_OUTER
-    meta_stopping_tol = META_STOPPING_TOL
-    save_figs = SAVE_FIGS
+    a = torch.tensor(args.a, requires_grad=True)
+    h = torch.tensor(args.h, requires_grad=True)
+    c = torch.tensor(args.c, requires_grad=False)
+    inner_steps = args.inner_steps
+    outer_steps = args.outer_steps
+    learning_rate = args.learning_rate
+    learning_rate_outer = args.learning_rate_outer
+    optimizer_name = args.optimizer
+    optimizer_outer_name = args.optimizer_outer
+    save_figs = args.save_figs == 'True'
+    use_wandb = args.use_wandb == 'True'
+    stopping_tol_outer = args.stopping_tol_outer
 
-    a = torch.tensor(A, requires_grad=True)
-    h = torch.tensor(H, requires_grad=True)
-    c = torch.tensor(C, requires_grad=False)
+    if use_wandb == True:
+        wandb.init(entity="uclnlp", project="kbc_meta", group=f"toymeta2")
+        wandb.config.update(args)
 
     device = torch.device('cpu')
 
@@ -55,11 +91,14 @@ def main():
     a_vals = []
     h_vals = []
     c_vals = []
-    check_converge_flag = True
+    converged = False
+    convergence_outer_step = None
+    convergence_total_steps = None
 
     h_graph = deepcopy(h)
 
     h_grads = []
+
     # def mult_grad(grad):
     #     return grad
     def store_h_grad(grad):
@@ -67,7 +106,6 @@ def main():
 
     # h_graph.register_hook(mult_grad)
     h_graph.register_hook(store_h_grad)
-
 
     optimizer_factory_outer = {
         'adagrad': lambda: optim.Adagrad([h_graph], lr=learning_rate_outer),
@@ -92,77 +130,97 @@ def main():
         diffopt = higher.get_diff_optim(optimizer, [a_graph], track_higher_grads=True)
 
         for inner_step in range(inner_steps):
-            loss = torch.norm(model.forward(a_graph)-h_graph)
+            loss = torch.norm(model.forward(a_graph) - h_graph)
             a_graph = diffopt.step(loss, params=[a_graph])
             a_graph = a_graph[0]
-            # print(a_graph)
 
         meta_loss = torch.norm(a_graph - c)
-        if meta_loss < meta_stopping_tol and check_converge_flag == True:
-            print(f"First converged at outer step {outer_step} (total of {outer_step*inner_steps} inner steps), with tolerance {meta_stopping_tol}")
-            check_converge_flag = False
+        if meta_loss < stopping_tol_outer and converged == False:
+            convergence_outer_step = outer_step
+            convergence_total_steps = outer_step * inner_steps
+            converged = True
 
-        # metrics for plotting
+        # Metrics for logging
         meta_losses += [meta_loss.detach().clone().item()]
         a_vals += [a_graph.detach().clone().item()]
         h_vals += [h_graph.detach().clone().item()]
         c_vals += [c.detach().clone().item()]
 
         meta_loss.backward()
+        if use_wandb == True:
+            train_log = {'meta_loss': meta_losses[-1],
+                         'a_value': a_vals[-1],
+                         'h_value': h_vals[-1],
+                         'c_value': c_vals[-1],
+                         'outer_convergence_value': np.absolute(h_vals[-1] - c_vals[-1]),
+                         'convergence_outer_step': convergence_outer_step,
+                         'convergence_total_steps': convergence_total_steps,
+                         'hypergradient_h': h_grads[-1]}
+            wandb.log(train_log, step=outer_step)
+
         optimizer_outer.step()
         optimizer_outer.zero_grad()
 
+    plt.figure(1)
     plt.plot(meta_losses)
     plt.plot(a_vals)
     plt.plot(h_vals)
     plt.plot(c_vals)
-    plt.legend(["meta loss","a","h","c"])
+    plt.legend(["meta loss", "a", "h", "c"])
     plt.xlabel("Outer step")
     plt.ylabel("Value as specified in legend")
-    plt.title(f"Starting values: a={A}, h={H}, c={C}\nInner steps: {inner_steps} | LR: {learning_rate} | Outer steps: {outer_steps} | OuterLR: {learning_rate_outer} | Optim: {optimizer_name}")
+    plt.title(
+        f"Starting values: a={args.a}, h={args.h}, c={args.c}\nInner steps: {inner_steps} | LR: {learning_rate} | Outer steps: {outer_steps} | OuterLR: {learning_rate_outer} \nOptim: {optimizer_name} | Converges step: {convergence_outer_step} | Converges total steps: {convergence_total_steps}")
     plt.tight_layout()
     if save_figs == True:
-        plt.savefig(f"./plots/toymeta2_loss_a{A}_h{H}_c{C}_is{inner_steps}_lr{learning_rate}_os{outer_steps}_outlr{learning_rate_outer}_optim{optimizer_name}.png")
-    plt.show()
+        filename = f"toymeta2_loss_a{args.a}_h{args.h}_c{args.c}_is{inner_steps}_lr{learning_rate}_os{outer_steps}_outlr{learning_rate_outer}_optim{optimizer_name}.png"
+        if use_wandb == True:
+            plt.savefig(os.path.join(wandb.run.dir, filename))
+        else:
+            plt.savefig(f"./toymeta2/plots/{filename}")
+
+    # plt.show()
 
     df_grad = pd.DataFrame(h_grads)
     rolling_mean_grads = df_grad.rolling(window=40).mean()
+    plt.figure(2)
     plt.plot(rolling_mean_grads)
     plt.title("Rolling 40-epoch mean gradient value")
     plt.xlabel("Outer step")
     plt.ylabel("Gradient of loss wrt hyperparameter h")
     plt.tight_layout()
     if save_figs == True:
-        plt.savefig(f"./plots/toymeta2_grad_rollingavg_a{A}_h{H}_c{C}_is{inner_steps}_lr{learning_rate}_os{outer_steps}_outlr{learning_rate_outer}_optim{optimizer_name}.png")
-    plt.show()
+        filename = f"toymeta2_grad_rollingavg_a{args.a}_h{args.h}_c{args.c}_is{inner_steps}_lr{learning_rate}_os{outer_steps}_outlr{learning_rate_outer}_optim{optimizer_name}.png"
+        if use_wandb == True:
+            plt.savefig(os.path.join(wandb.run.dir, filename))
+        else:
+            plt.savefig(f"./toymeta2/plots/{filename}")
+    # plt.show()
 
     array_grads = np.array(h_grads)
+    plt.figure(3)
     plt.hist(np.absolute(array_grads), bins=80)
-    plt.title(f" Magnitude of hypergradient of loss wrt hyperparameter h \nMean: {np.mean(np.absolute(array_grads)):.10f} | SD: {np.std(h_grads):.10f} | Median {np.median(np.absolute(array_grads)):.10f}")
+    plt.title(
+        f" Magnitude of hypergradient of loss wrt hyperparameter h \nMean: {np.mean(np.absolute(array_grads)):.10f} | SD: {np.std(h_grads):.10f} | Median {np.median(np.absolute(array_grads)):.10f}")
     plt.ylabel("Frequency")
     plt.xlabel("Hypergradient magnitude")
     plt.tight_layout()
     if save_figs == True:
-        plt.savefig(f"./plots/toymeta2_grad_hist_a{A}_h{H}_c{C}_is{inner_steps}_lr{learning_rate}_os{outer_steps}_outlr{learning_rate_outer}_optim{optimizer_name}.png")
-    plt.show()
+        filename = f"toymeta2_grad_gradhist_a{args.a}_h{args.h}_c{args.c}_is{inner_steps}_lr{learning_rate}_os{outer_steps}_outlr{learning_rate_outer}_optim{optimizer_name}.png"
+        if use_wandb == True:
+            plt.savefig(os.path.join(wandb.run.dir, filename))
+        else:
+            plt.savefig(f"./toymeta2/plots/{filename}")
+    # plt.show()
 
-    debug=0
-
+    if use_wandb == True:
+        train_log.update({'hypergrad_mean_magnitude': np.mean(np.absolute(array_grads)),
+                          'hypergrad_SD': np.std(h_grads),
+                          'hypergrad_median_magnitude': np.median(np.absolute(array_grads))})
+        wandb.run.summary.update(train_log)
+    wandb.finish()
 
 
 if __name__ == '__main__':
-
-    # Specify experimental parameters
-    OUTER_STEPS = 100
-    INNER_STEPS = 400
-    LEARNING_RATE = 0.01
-    LEARNING_RATE_OUTER = 0.01
-    OPTIMIZER = "adam"
-    OPTIMIZER_OUTER = "adam"
-    SAVE_FIGS = False
-    META_STOPPING_TOL = 0.02
-    A = 0.25
-    H = 0.28
-    C = 0.51
-
-    main()
+    args = parse_args(sys.argv[1:])
+    main(args)
