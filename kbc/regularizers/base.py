cba2e55fd2644edb1d03669d13de9e7e291cb187
Ravi Patel <79413022+ravipatelxyz@users.noreply.github.com>
2021-07-20 22:07:28 +0100

    added weighted_f2 and weighted_n3

--- kbc/regularizers/base.py
+++ kbc/regularizers/base.py
@@ -54,3 +54,31 @@ class Entropy(Regularizer):
             factors = [torch.softmax(f, dim=1) for f in factors]
         res = sum(torch.sum(- torch.log(f) * f) for f in factors)
         return res / factors[0].shape[0]
+
+
+def weighted_f2(lmbda_lhs, lmbda_rel, lmbda_rhs, factors):
+    lhs, rel, rhs = factors
+    l_reg = ((lmbda_lhs * (lhs ** 2)).sum() / lhs.shape[0]
+             + (lmbda_rel * (rel ** 2)).sum() / rel.shape[0]
+             + (lmbda_rhs * (rhs ** 2)).sum() /rhs.shape[0])
+    # l_reg = l_reg / factors[0].shape[0]
+    l_reg_raw = ((lhs.abs() ** 2).sum() / lhs.shape[0]
+                 + (rel.abs() ** 2).sum() / rel.shape[0]
+                 + (rhs.abs() ** 2).sum() /rhs.shape[0])
+    # l_reg_raw = l_reg_raw / factors[0].shape[0]
+    lmbda_avg = (lmbda_lhs.mean() + lmbda_rel.mean() + lmbda_rhs.mean()) / 3
+    return l_reg, l_reg_raw, lmbda_avg
+
+
+def weighted_n3(lmbda_lhs, lmbda_rel, lmbda_rhs, factors):
+    lhs, rel, rhs = factors
+    l_reg = ((lmbda_lhs * (lhs.abs() ** 3)).sum() / lhs.shape[0]
+             + (lmbda_rel * (rel.abs() ** 3)).sum() / rel.shape[0]
+             + (lmbda_rhs * (rhs.abs() ** 3)).sum() / rhs.shape[0])
+    # l_reg = l_reg / factors[0].shape[0]
+    l_reg_raw = ((lhs.abs() ** 3).sum() / lhs.shape[0]
+                + (rel.abs() ** 3).sum() / rel.shape[0]
+                + (rhs.abs() ** 3).sum() / rhs.shape[0])
+    # l_reg_raw = l_reg_raw / factors[0].shape[0]
+    lmbda_avg = (lmbda_lhs.mean() + lmbda_rel.mean() + lmbda_rhs.mean()) / 3
+    return l_reg, l_reg_raw, lmbda_avg
